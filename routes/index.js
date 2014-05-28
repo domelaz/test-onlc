447cf8ea4ffc19c9b047b8b613c50e4b4a33b3d6
Антон Домолазов <anton@domelaz.ru>
2014-05-28 12:23:51 +0400

    search route added

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
-
+// Return search page in html
 exports.index = function(req, res) {
 	res.render('page');
 };	
+
+// Return search results in json
+exports.search = function(req, res) {
+	res.send(500);
+};
