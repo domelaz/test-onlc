fc15eb6740d88705119bd45dff3c6c4376ad0158
Антон Домолазов <anton@domelaz.ru>
2014-05-31 10:36:42 +0400

    grunt jshint task added

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,37 @@ module.exports = function(grunt) {
 				dest: 'public/js/scripts.js'
 			}
 		},
+		jshint: {
+			options: {
+				camelcase: true,
+				curly: true,
+				eqeqeq: true,
+				indent: true,
+				latedef: true,
+				node: true,
+				trailing: true,
+				undef: true,
+				unused: true,
+				predef: [
+					'angular'
+				]
+			},
+			app: {
+				src: ['public/js/onlc.js']
+			},
+			node: {
+				src: [
+					'lib/**/*.js',
+					'routes/**/*.js'
+				]
+			},
+			gruntfile: {
+				src: ['Gruntfile.js']
+			}
+		},
 	});
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.registerTask('glue', ['concat']);
+	grunt.registerTask('hint', ['jshint']);
 };
